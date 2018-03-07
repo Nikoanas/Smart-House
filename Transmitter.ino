5c465a32aebe962a441c434541036359f5fdbe3a
Nikoanas <nikoanas4@gmail.com>
2018-03-07 18:04:43 +0200

    Add files via upload

---
+++ Transmitter.ino
@@ -0,0 +1,59 @@
+#include <DigitalIO.h>
+#include <DigitalPin.h>
+#include <I2cConstants.h>
+#include <PinIO.h>
+#include <SoftI2cMaster.h>
+#include <SoftSPI.h>
+
+#include  <SPI.h>
+#include "RF24.h"
+#include "Wire.h"
+
+String msg;
+char lol;
+RF24 radio(7,8);
+const uint64_t pipe =  0xE8E8F0F0E1LL;
+//const uint64_t pipe2 = 0xB3B4B5B6CDLL;
+
+
+void setup(void){
+ Serial.begin(57600);
+ radio.begin();
+ radio.setPALevel(RF24_PA_LOW);
+ radio.openWritingPipe(pipe);
+// radio.openWritingPipe(pipe2);
+ radio.stopListening();}
+
+void loop(void){
+
+
+if (Serial.available()>0) {
+  delay(10);
+
+  
+while (Serial.available()>0){
+  lol= Serial.read();
+  msg +=lol;  }
+
+int leng=msg.length();
+Serial.print(msg);
+Serial.println(leng);
+//bool ok = radio.write( &lol, 1 ); 
+bool ok = sendData(msg);
+Serial.println(ok);
+msg="";
+ 
+
+ 
+
+/*
+ while (Serial.available()>0){
+ char lol= Serial.read();
+ bool ok = radio.write( &lol, 1 );
+ delay(50);
+*/
+ 
+  }
+}
+
+ 
---
+++ receiver.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+char msg;
+RF24 radio(7,8);
+const uint64_t pipe1 = 0xE8E8F0F0E1LL;    // Node1
+const uint64_t pipe2 = 0xE8E8F0F0E1F1;
+byte Numpipe[]={0, 1};
+
+
+void setup(void){
+ Serial.begin(9600);
+ radio.begin();
+ radio.setPALevel(RF24_PA_MAX);
+ radio.openReadingPipe(0,pipe1);
+ radio.openReadingPipe(1,pipe2);
+ radio.startListening();
+ 
+ 
+}
+
+void loop(void){
+ 
+  
+   while (radio.available()) {
+     String data= getData();
+     
+     //radio.read(&msg, 1);     
+     Serial.println(data);
+     delay(10);
+     }
+     
+  
+
+     }
